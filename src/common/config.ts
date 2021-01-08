0559dc37e4c341c6f48c3b4bc9cfdf9795d75cc7
lyqhn2012 <33479488+YaqiLee@users.noreply.github.com>
2021-01-08 17:57:42 +0800

    add 测试文件

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,13 +1,3 @@
-export const brush = {
-  select: "select", // 选区
-  curve: "curve",
-  line: "line",
-  rect: "rect", // 填充
-  oRect: "oRect", // 空心矩形
-  circle: "circle", // 填充圆
-  oCircle: "oCircle", // 空心圆
-};
-
 export enum BrushShape {
   select,
   curve,
